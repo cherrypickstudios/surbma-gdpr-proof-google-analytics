ff51c430874012224f75abded7a9c528b21f887e
Surbma <peter@surbma.hu>
2023-03-25 08:13:55 +0100

    17.5.5
    OTHER
    
    - Minor code optimizations.

--- lib/shortcodes.php
+++ lib/shortcodes.php
@@ -1,15 +1,14 @@
 <?php
 
-function surbma_gpga_modal_link_shortcode( $atts ) {
+add_shortcode( 'surbma-cookie-popup-link', function( $atts ) {
 	extract( shortcode_atts( array(
 		'class' => '',
 		'text' => 'Open Cookie Settings'
 	), $atts ) );
 	return '<a class="surbma-gpga-cookie-popup-link '.$class.'" onclick="surbma_gpga_openModal();">'.$text.'</a>';
-}
-add_shortcode( 'surbma-cookie-popup-link', 'surbma_gpga_modal_link_shortcode' );
+} );
 
-function surbma_gpga_cookie_scan_shortcode( $atts ) {
+add_shortcode( 'surbma-live-cookie-scan', function( $atts ) {
 	extract( shortcode_atts( array(
 		'cookievalue' => 'true'
 	), $atts ) );
@@ -31,11 +30,9 @@ function surbma_gpga_cookie_scan_shortcode( $atts ) {
 		$cookieScan = '<p>' . __( 'COOKIE is not set on this website.', 'surbma-gdpr-proof-google-analytics' ) . '</p>';
 	}
 return $cookieScan;
-}
-add_shortcode( 'surbma-live-cookie-scan', 'surbma_gpga_cookie_scan_shortcode' );
+} );
 
-function surbma_gpga_social_buttons_shortcode() {
+add_shortcode( 'surbma-social-buttons', function() {
 	wp_enqueue_style( 'surbma-gpga-social-share' );
 	return surbma_gpga_social_add_share_buttons();
-}
-add_shortcode( 'surbma-social-buttons', 'surbma_gpga_social_buttons_shortcode' );
+} );
